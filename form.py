9bb773b27bdc024897116d5d577e4c3ceb638919
Nether190 <valtinhomn@gmail.com>
2021-07-26 14:43:08 -0300

    Antes de compilar

--- form.py
+++ form.py
@@ -1,12 +1,66 @@
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+from time import strftime, sleep
+import codecs
+
+#Lê o horário e atualiza [6] de dados.txt com o nome da aula atual
+
+wb = load_workbook('horario.xlsx')
+ws = wb.active
+
+tabela = {'Mon':1,'Tue':4,'Wed':7,'Thu':10,'Fri':13}
+
+hora = int(strftime('%H%M'))
+dia = strftime('%a')
+
+with codecs.open('dados.txt', encoding='utf-8', mode='r') as ler:
+    Dados = ler.read().splitlines()
+
+Camil = Dados[5]
+
+diaHora = tabela.get(dia)
+if Camil: diaAula = diaHora+1
+else: diaAula = diaHora+2
+
+
+
+def compara_hora(CH,CA,C,F):
+    letra = get_column_letter(CH)
+    aula = get_column_letter(CA)
+    for row in range(C,F+1):
+        valor = ws[letra+str(row)].value
+        if hora <= valor:
+            return(ws[aula+str(row-1)].value)
+        else: pass
+
+
+aulaAtual = compara_hora(diaHora,diaAula,2,11).encode('utf-8')
+
+
+with  open("dados.txt", "r") as arquivo:
+    linhas = arquivo.readlines()
+
+if len(linhas) == 7:
+    del linhas[6]
+
+with open("dados.txt", "w+") as arquivoNovo:
+    for linha in linhas:
+        arquivoNovo.write(linha)
+
+with open('dados.txt', 'ab') as nota:
+    nota.write(aulaAtual)
+
+
+#Usa o Selenium para abrir o formulário e preencher com os dados do usuário
+
 from selenium import webdriver
-import time
 import codecs
 from tkinter import messagebox, Tk
 
 browser = webdriver.Chrome('chromedriver')
 browser.get('https://docs.google.com/forms/d/e/1FAIpQLSf8IpgwW4DcIsvrKMp-UhffOTP5MMiSCd5ttxVvK6pFXXbwCA/viewform')
 
-time.sleep(1.5)
+sleep(1.5)
     
 with codecs.open('dados.txt', encoding='utf-8', mode='r') as ler:
     Dados = ler.read().splitlines()
@@ -63,7 +117,7 @@ inputAula = browser.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/d
 inputAula.click()
 
 
-time.sleep(0.5)
+sleep(0.5)
 try:
     inputAula = browser.find_element_by_xpath(f'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div[2]/div[{idCaixa.get(Aula)}]/span')
 except: 
@@ -75,7 +129,7 @@ except:
     
 inputAula.click()
 
-time.sleep(2)
+sleep(2)
 
 enviar = browser.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[3]/div/div/span')
 enviar.click()
--- main.bat
+++ main.bat
@@ -1,4 +1,3 @@
 @ECHO OFF
 PowerShell.exe -windowstyle hidden -Command "& & env/Scripts/Activate.ps1"
-PowerShell.exe -windowstyle hidden -Command "& env/Scripts/python.exe ler_horario.py"
 PowerShell.exe -windowstyle hidden -Command "& env/Scripts/python.exe form.py"
