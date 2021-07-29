c419e4847028452cb239e25eb8f218fd90acaa7f
Nether190 <valtinhomn@gmail.com>
2021-07-29 13:10:21 -0300

    commit

--- ler_horario.py
+++ ler_horario.py
@@ -8,31 +8,31 @@ ws = wb.active
 
 tabela = {'Mon':1,'Tue':4,'Wed':7,'Thu':10,'Fri':13}
 
-hora = int(strftime('%H%M'))
-dia = strftime('%a')
 
 with codecs.open('dados.txt', encoding='utf-8', mode='r') as ler:
     Dados = ler.read().splitlines()
 
 Camil = Dados[5]
 
-diaHora = tabela.get(dia)
-if Camil: diaAula = diaHora+1
-else: diaAula = diaHora+2
+colunaHora = tabela.get(strftime('%a'))
+if Camil: colunaAula = colunaHora+1
+else: colunaAula = colunaHora+2
 
 
+def compara_hora(CH,CA):
+    hora = 650
 
-def compara_hora(CH,CA,C,F):
-    letra = get_column_letter(CH)
-    aula = get_column_letter(CA)
-    for row in range(C,F+1):
-        valor = ws[letra+str(row)].value
-        if hora <= valor:
-            return(ws[aula+str(row-1)].value)
-        else: pass
+    for row in range(2,13):
+        horario = ws[get_column_letter(CH)+str(row)].value
+
+        if ws[get_column_letter(CA)+str(row)].value == None: 
+            return ws[get_column_letter(CA)+str(row-1)].value
+
+        elif hora > horario: pass
+        else: return ws[get_column_letter(CA)+str(row)].value
 
 
-aulaAtual = compara_hora(diaHora,diaAula,2,11).encode('utf-8')
+aulaAtual = compara_hora(colunaHora,colunaAula).encode('utf-8')
 
 
 with  open("dados.txt", "r") as arquivo:
